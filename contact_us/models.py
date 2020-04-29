cdb47fd5c33b363aea746ced50ac5dfaa80fb0ba
rahul-raj007 <rahulraj99ex@gamil.com>
2020-04-29 11:43:05 +0530

    created ContactMessage

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ContactMessage(models.Model):
+    full_name = models.CharField(max_length=225)
+    email = models.CharField(max_length=225)
+    contact_message = models.TextField()
+    creations_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
