b8002f9515cf12305ab33e73be58a190fe276c8a
rahul-raj007 <rahulraj99ex@gamil.com>
2020-04-29 11:42:27 +0530

    pcreated contact_message_creations view

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import ContactMessageForm
+# from contact_us.models import ContactMessage
 
-# Create your views here.
+
+def contact_message_creations(request):
+    if request.method == "POST":
+        form = ContactMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ContactMessageForm()
+    context = {"form": form}
+
+    return render(request, "contact_us/contact_us.html", context)
+
